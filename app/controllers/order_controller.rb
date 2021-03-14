5a9af52a5914143595951b6798a6ba0d17325b2b
Bhavani <bhavanivamshee@gmail.com>
2021-03-14 16:57:17 -0500

    adds show page for orders

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -5,7 +5,18 @@ class OrderController < ApplicationController
     end
 
     post '/orders' do
-        "process our form"
+        @order = Order.create(
+            item: params[:item], 
+            item_price: params[:item_price], 
+            total: params[:total] 
+        )
+        redirect "/orders/#{@order.id}"
+
+    end
+
+    get '/orders/:id' do
+        @order = Order.find(params[:id])
+        erb :'/orders/show'
     end
 
 end
