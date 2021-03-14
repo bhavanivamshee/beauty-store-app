79ccdab5fa762c518483282940334db05fb3a264
Bhavani <bhavanivamshee@gmail.com>
2021-03-14 15:25:53 -0500

    adds show page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,12 @@ class UserController < ApplicationController
 
     post '/users/signup' do
         @user = User.create(username: params[:username], password: params[:password])
-        binding.pry
+        redirect "/users/#{@user.id}"
+    end
+
+    get '/users/:id' do 
+        @user = User.find(params[:id])
+        erb :'/users/show'
     end
 
 
